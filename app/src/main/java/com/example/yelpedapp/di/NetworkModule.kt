04b79615a6e176ce3bbdeee876d301c7fcfd3956
waliahimanshu <himanshu.walia@gmail.com>
2021-07-25 07:40:57 +0100

    added koin network module to provide retrofit

---
+++ app/src/main/java/com/example/yelpedapp/di/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.example.yelpedapp.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+
+private const val BASE_UR = "https://api.yelp.com/v3/"
+
+val networkModule = module {
+
+    single {
+        provideHttpClient(provideLoggingInterceptor())
+    }
+
+    single {
+        provideRetrofit(httpClient = get(), moshi = provideMoshi())
+    }
+
+}
+
+fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply {
+    setLevel(HttpLoggingInterceptor.Level.BODY)
+}
+
+fun provideMoshi(): Moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
+fun provideRetrofit(httpClient: OkHttpClient, moshi: Moshi) {
+    Retrofit.Builder()
+        .baseUrl(BASE_UR)
+        .client(httpClient)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+}
+
+fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor) {
+    OkHttpClient.Builder()
+        .addInterceptor(httpLoggingInterceptor)
+        .build()
+}
